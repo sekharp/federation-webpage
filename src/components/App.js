e5068873e0198948f062b2d147d7a3e7d89fe20a
Sekhar Paladugu <sekharp@gmail.com>
2016-04-19 15:54:02 -0600

    placeholders rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
 import '../assets/stylesheets/base.scss';
 import React, { Component } from 'react';
 
-const movieName = "Federation"
-
+import Navbar from './navbar'
 import Header from './header';
+import Article from './article';
+import Image from './image';
+import Footer from './footer';
+
+const movieName = "Federation"
 
 const App = React.createClass({
   render() {
     return(
-      <Header title={movieName} />
+      <div>
+        <Navbar />
+        <Header title={movieName} />
+        <Article />
+        <Image />
+        <Footer />
+      </div>
     )
   }
 });
