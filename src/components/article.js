ac327abdbe0cbab280d8e8d7ce2e27ffb70a13b7
Sekhar Paladugu <sekharp@gmail.com>
2016-04-22 12:39:50 -0600

    partial solution to article component stacking done

--- src/components/article.js
+++ src/components/article.js
@@ -5,12 +5,13 @@ const Article = React.createClass({
     return(
       <div className="article-div">
         <div className="row">
-            <div className="col-md-6">
+            <div className="col-md-4">
                 <img className="article-img" src="http://i.imgur.com/FTMRiSp.png"/>
             </div>
-            <div className="col-md-6">
+            <div className="col-md-8">
               <h5><b>Video: A Growing Number of Cylons are Feeling Misunderstood</b></h5>
-              <p>Lorem Ipsum</p>
+              <p>Lorem Ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <p>Lorem Ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>              
             </div>
         </div>
       </div>
