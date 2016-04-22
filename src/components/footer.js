32f7e5e6ec786df536c47faa9124e1c3ce9c2e2e
Sekhar Paladugu <sekharp@gmail.com>
2016-04-22 13:44:22 -0600

    footer div pieces left borders working

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,19 +5,28 @@ const Footer = React.createClass({
     return(
       <div className="footer-div">
         <div className="row">
-          <div className="col-md-6">
-            <h5 className="white-text">About Us</h5>
+          <div className="col-md-4 footer-div-piece">
+            <h5 className="footer-title-text">In The News</h5>
             <ul>
-              <li><a className="grey-text text-lighten-3" href="#!">Overview</a></li>
-              <li><a className="grey-text text-lighten-3" href="#!">How It Works</a></li>
+              <li><a className="footer-body-text" href="#!">Cable TV Blackout in the Twelve Colonies</a></li>
+              <li><a className="footer-body-text" href="#!">Earth Appears Not to be Where it Was Before</a></li>
+              <li><a className="footer-body-text" href="#!">Cloud 9 Closed for Renovation</a></li>
             </ul>
           </div>
-          <div className="col-md-6">
-            <h5 className="white-text">Links</h5>
+
+          <div className="col-md-4 footer-div-piece">
+            <h5 className="footer-title-text">Event Calendar</h5>
             <ul>
-              <li><a className="grey-text text-lighten-3" href="#!">Lend</a></li>
-              <li><a className="grey-text text-lighten-3" href="#!">Contact</a></li>
-              <li><a className="grey-text text-lighten-3" href="#!">Developers</a></li>
+              <li><a className="footer-body-text" href="#!">Caprican Bake Sale</a></li>
+              <li><a className="footer-body-text" href="#!">Annual Sixes and Eights Mixer</a></li>
+              <li><a className="footer-body-text" href="#!">2015 Raptor Maintenance and Repair Conference</a></li>
+            </ul>
+          </div>
+
+          <div className="col-md-4">
+            <h5 className="footer-title-text">So Say We All</h5>
+            <ul>
+              <li><a className="grey-text text-lighten-3" href="#!">When is it appropriate to high-five a commander?</a></li>
             </ul>
           </div>
         </div>
