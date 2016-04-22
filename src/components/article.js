8042e968da05dce8b6f0cd9ad069438a73a431b8
Sekhar Paladugu <sekharp@gmail.com>
2016-04-22 12:09:07 -0600

    article text and img rendering but components not stacking

--- src/components/article.js
+++ src/components/article.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 const Article = React.createClass({
   render() {
     return(
-      <h1>Article Placeholder</h1>
+      <div className="article-div">
+        <div className="row">
+            <div className="col-md-6">
+                <img className="article-img" src="http://i.imgur.com/FTMRiSp.png"/>
+            </div>
+            <div className="col-md-6">
+              <h5><b>Video: A Growing Number of Cylons are Feeling Misunderstood</b></h5>
+              <p>Lorem Ipsum</p>
+            </div>
+        </div>
+      </div>
     )
   }
 });
