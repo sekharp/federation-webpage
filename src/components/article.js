67a33ba808d18ba884d0fbf1f27af6a90668e6d8
Sekhar Paladugu <sekharp@gmail.com>
2016-04-22 12:43:30 -0600

    small changes to article text padding

--- src/components/article.js
+++ src/components/article.js
@@ -8,10 +8,10 @@ const Article = React.createClass({
             <div className="col-md-4">
                 <img className="article-img" src="http://i.imgur.com/FTMRiSp.png"/>
             </div>
-            <div className="col-md-8">
+            <div className="col-md-8 article-text">
               <h5><b>Video: A Growing Number of Cylons are Feeling Misunderstood</b></h5>
               <p>Lorem Ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-              <p>Lorem Ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>              
+              <p>Lorem Ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             </div>
         </div>
       </div>
