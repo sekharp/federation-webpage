0690a47b55c64908aafe3e33daa9e92bbf35c1f4
Sekhar Paladugu <sekharp@gmail.com>
2016-04-22 13:59:16 -0600

    footer mostly set, small css changes needed

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,28 +6,29 @@ const Footer = React.createClass({
       <div className="footer-div">
         <div className="row">
           <div className="col-md-4 footer-div-piece">
-            <h5 className="footer-title-text">In The News</h5>
-            <ul>
-              <li><a className="footer-body-text" href="#!">Cable TV Blackout in the Twelve Colonies</a></li>
-              <li><a className="footer-body-text" href="#!">Earth Appears Not to be Where it Was Before</a></li>
-              <li><a className="footer-body-text" href="#!">Cloud 9 Closed for Renovation</a></li>
-            </ul>
+            <h5 className="footer-title-text">IN THE NEWS</h5>
+            <a className="footer-body-text" href="#!">Cable TV Blackout in the Twelve Colonies</a><br />
+            <span className="glyphicon glyphicon-calendar" aria-hidden="true" /><a className="footer-date-text" href="#!"> Tuesday, September 16, 2014</a><br />
+            <a className="footer-body-text" href="#!">Earth Appears Not to be Where it Was Before</a><br />
+            <span className="glyphicon glyphicon-calendar" aria-hidden="true" /><a className="footer-date-text" href="#!"> Tuesday, September 16, 2014</a><br />
+            <a className="footer-body-text" href="#!">Cloud 9 Closed for Renovation</a><br />
+            <span className="glyphicon glyphicon-calendar" aria-hidden="true" /><a className="footer-date-text" href="#!"> Tuesday, September 16, 2014</a>
           </div>
 
           <div className="col-md-4 footer-div-piece">
-            <h5 className="footer-title-text">Event Calendar</h5>
-            <ul>
-              <li><a className="footer-body-text" href="#!">Caprican Bake Sale</a></li>
-              <li><a className="footer-body-text" href="#!">Annual Sixes and Eights Mixer</a></li>
-              <li><a className="footer-body-text" href="#!">2015 Raptor Maintenance and Repair Conference</a></li>
-            </ul>
+            <h5 className="footer-title-text">EVENT CALENDAR</h5>
+            <a className="footer-body-text" href="#!">Caprican Bake Sale</a><br />
+            <span className="glyphicon glyphicon-calendar" aria-hidden="true" /><a className="footer-date-text" href="#!"> Tuesday, September 16, 2014</a><br />
+            <a className="footer-body-text" href="#!">Annual Sixes and Eights Mixer</a><br />
+            <span className="glyphicon glyphicon-calendar" aria-hidden="true" /><a className="footer-date-text" href="#!"> Tuesday, September 16, 2014</a><br />
+            <a className="footer-body-text" href="#!">2015 Raptor Maintenance and Repair Conference</a><br />
+            <span className="glyphicon glyphicon-calendar" aria-hidden="true" /><a className="footer-date-text" href="#!"> Tuesday, September 16, 2014</a>
           </div>
 
           <div className="col-md-4">
-            <h5 className="footer-title-text">So Say We All</h5>
-            <ul>
-              <li><a className="grey-text text-lighten-3" href="#!">When is it appropriate to high-five a commander?</a></li>
-            </ul>
+            <h5 className="footer-title-text">SO SAY WE ALL</h5>
+            <img src="http://i.imgur.com/VvpVAcY.png" />
+            <a className="footer-body-text">When is it appropriate to high-five a commander?</a>
           </div>
         </div>
       </div>
